f7a08317cf98e17a2ee0f94d751df1587a5547c0
Herick Bittencourt <professional.herickvvb@gmail.com>
2025-03-09 08:37:26 -0300

    Feat: Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Trabalho M1.1
+// Grafos 2025 - 1
+// Alunos: 
+//      Hérick Vitor Vieira Bittencourt
+//      Rodrigo Rey de Oliveira Carrard
+//      Paola Piran Zanella
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
